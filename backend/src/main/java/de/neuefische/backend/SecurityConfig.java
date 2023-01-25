dfd9768cb3a204b2344f97f410bc10dca31b2abb
thanhduong <xuanthanhduong94@gmail.com>
2023-01-25 14:04:55 +0100

    add: @WithMockUser

--- backend/src/main/java/de/neuefische/backend/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .csrf().disable()
                 .httpBasic().and()
                 .authorizeHttpRequests()
-                .antMatchers(HttpMethod.POST,"/api/app-users/login").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/app-users").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
--- backend/src/test/java/de/neuefische/backend/user/AppUserControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/user/AppUserControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,9 +19,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AppUserControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
     @Test
-
+    @WithMockUser(username = "user")
     void create_WhenLoggedInThenCreateUserSuccessfully() throws Exception {
         String given = """
                 {
@@ -64,7 +64,7 @@ class AppUserControllerTest {
     }
 
     @Test
-    void me() {
+    void me_WhenLoggedInReturn() {
     }
 
     @Test
