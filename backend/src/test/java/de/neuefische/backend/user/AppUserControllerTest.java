e560db5bdcc07e5461e9b4c4f1401397be3266c0
thanhduong <xuanthanhduong94@gmail.com>
2023-01-27 01:56:51 +0100

    feat: integration tests for app user controller

--- backend/src/test/java/de/neuefische/backend/user/AppUserControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/user/AppUserControllerTest.java
@@ -9,9 +9,9 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.hamcrest.Matchers.notNullValue;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,10 +19,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AppUserControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
     @Test
-    @WithMockUser(username = "user")
-    void create_WhenLoggedInThenCreateUserSuccessfully() throws Exception {
-        String given = """
+    void create_whenCreateUserSuccessfullyThenReturnCreatedAppUser() throws Exception {
+        String requestBody = """
                 {
                     "username": "user",
                     "password": "123"
@@ -36,38 +39,103 @@ class AppUserControllerTest {
                 """;
         mockMvc.perform(MockMvcRequestBuilders.post("/api/app-users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(given))
-                .andExpect(status().isOk())
-                .andExpect(content().json(expected))
-                .andExpect(jsonPath("$.id", notNullValue()));
-
+                        .content(requestBody))
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().json(expected));
     }
 
     @Test
-    void create_WhenNotLoggedInThen401() throws Exception {
-        String given = """
+    void create_whenAppUserAlreadyExistsThenConflict() throws Exception {
+        String requestBody = """
                 {
                     "username": "user",
                     "password": "123"
                 }
                 """;
+        String expected = """
+                {
+                    "username": "user",
+                    "password": ""
+                }
+                """;
         mockMvc.perform(MockMvcRequestBuilders.post("/api/app-users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(given))
-                .andExpect(status().isUnauthorized());
+                        .content(requestBody))
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().json(expected));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/app-users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isConflict());
 
     }
 
-
     @Test
-    void login() {
+    @WithMockUser(username = "user",password = "123")
+    void login_whenAppUserExistsThenReturn200AndAppUser() throws Exception {
+
+        appUserRepository.save(new AppUser("abc", "user", "123"));
+
+        String expected = """
+                {
+                    "id": "abc",
+                    "username": "user",
+                    "password": ""
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/app-users/login"))
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().json(expected));
     }
 
     @Test
-    void me_WhenLoggedInReturn() {
+    void login_whenAppUserNotExistThen401() throws Exception {
+        String requestBody = """
+                {
+                    "username": "user",
+                    "password": "123"
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/app-users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isUnauthorized());
     }
 
     @Test
-    void logout() {
+    @WithMockUser(username = "user",password = "123")
+    void me_whenLoggedInThenReturnAppUserData() throws Exception {
+
+        appUserRepository.save(new AppUser("abc", "user", "123"));
+
+        String expected = """
+                {
+                    "id": "abc",
+                    "username": "user",
+                    "password": ""
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/app-users/me"))
+               .andExpectAll(
+                       MockMvcResultMatchers.status().isOk(),
+                       MockMvcResultMatchers.content().json(expected));
+    }
+
+    @Test
+    void me_whenNotLoggedInThen401() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/app-users/me"))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "user",password = "123")
+    void logout_withRegisteredUserThen200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/app-users/logout"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
