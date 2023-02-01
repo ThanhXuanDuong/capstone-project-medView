ef0fc6a4385120a6feee6070eab70965420954bd
thanhduong <xuanthanhduong94@gmail.com>
2023-02-01 22:35:53 +0100

    feat: tests for GET/PUT/DELETE

--- backend/src/test/java/de/neuefische/backend/note/NoteControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/note/NoteControllerTest.java
@@ -1,5 +1,8 @@
 package de.neuefische.backend.note;
 
+import de.neuefische.backend.patient.Gender;
+import de.neuefische.backend.patient.Patient;
+import de.neuefische.backend.patient.PatientRepository;
 import de.neuefische.backend.user.AppUser;
 import de.neuefische.backend.user.AppUserRepository;
 import org.junit.jupiter.api.Test;
@@ -11,8 +14,10 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,6 +31,9 @@ class NoteControllerTest {
 
     @Autowired
     private AppUserRepository appUserRepository;
+
+    @Autowired
+    private PatientRepository patientRepository;
 
     @Autowired
     private NoteRepository noteRepository;
@@ -43,12 +51,14 @@ class NoteControllerTest {
 
         String requestBody = """
                 {
+                    "id": "1",
                     "imageId":"1",
                     "text":"note 1"
                 }
                 """;
         String expected = """
                 {
+                    "id": "1",
                     "imageId":"1",
                     "text":"note 1"
                 }
@@ -60,7 +70,7 @@ class NoteControllerTest {
                         .content(requestBody))
                 .andExpectAll(
                         status().isOk(),
-                        content().json(expected));
+                        content().json(expected,true));
     }
 
     @Test
@@ -71,7 +81,7 @@ class NoteControllerTest {
 
     @Test
     @WithMockUser(username = "user", password = "pw")
-    void getAllByImageId_returnEmptyList_whenLoggedInAndNoNoteExits () throws Exception {
+    void getAllByImageId_returnEmptyList_whenLoggedInAndNoNoteExists () throws Exception {
         appUserRepository.save(new AppUser("1", "user", "pw"));
 
         mvc.perform(get("/api/notes/image/imageId"))
@@ -106,6 +116,155 @@ class NoteControllerTest {
         mvc.perform(get("/api/notes/image/imageId"))
                 .andExpectAll(
                         status().isOk(),
-                        content().json(expected));
+                        content().json(expected,true));
+    }
+
+    @Test
+    void getAllByPatientId_return401_whenNotLoggedIn () throws Exception {
+        mvc.perform(get("/api/notes/patient/1"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "pw")
+    void getAllByPatientId_returnEmptyList_whenLoggedInAndNoNoteExists () throws Exception {
+        appUserRepository.save(new AppUser("1", "user", "pw"));
+
+        patientRepository.save(new Patient(
+                "1",
+                "Miller",
+                "Helen",
+                Gender.FEMALE,
+                "2000-10-20",
+                "",
+                "",
+                List.of(),
+                LocalDateTime.now(),
+                "user"
+        ));
+
+        mvc.perform(get("/api/notes/patient/1"))
+                .andExpectAll(
+                        status().isOk(),
+                        content().json("{}", true)
+                );
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "pw")
+    void getAllByPatientId_returnAllNotesOfAPatient_whenLoggedIn () throws Exception {
+        appUserRepository.save(new AppUser("1", "user", "pw"));
+
+        patientRepository.save(new Patient(
+                "1",
+                "Miller",
+                "Helen",
+                Gender.FEMALE,
+                "2000-10-20",
+                "",
+                "",
+                List.of("imageId1", "imageId2"),
+                LocalDateTime.now(),
+                "user"
+        ));
+
+        noteRepository.save(new Note("1","imageId1","note1"));
+        noteRepository.save(new Note("2","imageId1","note2"));
+        noteRepository.save(new Note("3","imageId3","note3"));
+
+        String expected = """
+                {
+                    "imageId1": [
+                         {
+                             "id": "1",
+                             "imageId": "imageId1",
+                             "text": "note1"
+                         },
+                         {
+                             "id": "2",
+                             "imageId": "imageId1",
+                             "text": "note2"
+                         }
+                     ],
+                    "imageId2": []
+                }
+                """;
+
+        mvc.perform(get("/api/notes/patient/1"))
+                .andExpectAll(
+                        status().isOk(),
+                        content().json(expected,true)
+                );
+    }
+
+    @Test
+    void updateById_return401_whenNotLoggedIn () throws Exception {
+        mvc.perform(put("/api/notes/1"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "pw")
+    void updateById_returnUpdatedNote_whenLoggedIn() throws Exception {
+        appUserRepository.save(new AppUser("1", "user", "pw"));
+
+        String note = """
+                {
+                    "id": "1",
+                    "imageId":"1",
+                    "text":"note 1"
+                }
+                """;
+        String updated = """
+                {
+                    "id": "1",
+                    "imageId":"1",
+                    "text":"modified note"
+                }
+                """;
+        String expected = """
+                {
+                    "id": "1",
+                    "imageId":"1",
+                    "text":"modified note"
+                }
+                """;
+
+        //when and then
+        mvc.perform(post("/api/notes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(note))
+                .andExpectAll(
+                        status().isOk(),
+                        content().json(note,true));
+
+        mvc.perform(put("/api/notes/1")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(updated))
+                .andExpectAll(
+                        status().isOk(),
+                        content().json(expected, true)
+                );
+    }
+
+    @Test
+    void deleteById_return401_whenNotLoggedIn () throws Exception {
+        mvc.perform(delete("/api/notes/1"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "pw")
+    void deleteById_return200_whenLoggedInAndDeleteNote() throws Exception {
+        appUserRepository.save(new AppUser("1","user","pw"));
+
+        noteRepository.save(new Note("1","imageId1","note1"));
+
+        mvc.perform(delete("/api/notes/1"))
+                .andExpect(status().isOk());
+
+        mvc.perform(get("/api/notes/image/imageId1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]",true));
     }
 }
--- backend/src/test/java/de/neuefische/backend/note/NoteServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/note/NoteServiceTest.java
@@ -1,14 +1,35 @@
 package de.neuefische.backend.note;
 
+import de.neuefische.backend.exception.NotFoundException;
+import de.neuefische.backend.exception.PatientNotRegisteredException;
+import de.neuefische.backend.patient.Gender;
+import de.neuefische.backend.patient.Patient;
 import de.neuefische.backend.patient.PatientRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
 class NoteServiceTest {
+    private Patient createOnePatient(){
+        return  new Patient(
+                 "patientId",
+                "Mustermann",
+                "Max",
+                Gender.MALE,
+                "2000-10-20",
+                "test address",
+                "test email",
+                List.of("testImageId"),
+                LocalDateTime.of(2023, 1, 1, 0, 0),
+                "user1"
+        );
+    }
 
     @Test
     void add_addNoteToRepoAndReturnAddedNote() {
@@ -32,9 +53,10 @@ class NoteServiceTest {
     void getAllByImageId_getAllNotesOfAnImage() {
         //given
         Note note1 = new Note("testId 1","imageId 1","testText");
+        Note note2 = new Note("testId 2","imageId 1","testText2");
 
         NoteRepository noteRepository = mock(NoteRepository.class);
-        when(noteRepository.findAllByImageId("imageId 1")).thenReturn(List.of(note1));
+        when(noteRepository.findAllByImageId("imageId 1")).thenReturn(List.of(note1,note2));
 
         PatientRepository patientRepository = mock(PatientRepository.class);
 
@@ -43,7 +65,112 @@ class NoteServiceTest {
         List<Note> actual = noteService.getAllByImageId("imageId 1");
 
         //then
-        Assertions.assertEquals(List.of(note1), actual);
+        Assertions.assertEquals(List.of(note1,note2), actual);
         verify(noteRepository).findAllByImageId("imageId 1");
+    }
+
+    @Test
+    void getAllByPatientId_throwException_whenPatientNotRegistered() {
+        //given
+        NoteRepository noteRepository = mock(NoteRepository.class);
+        PatientRepository patientRepository = mock(PatientRepository.class);
+
+        //when
+        NoteService noteService = new NoteService(noteRepository,patientRepository);
+
+        //then
+        Assertions.assertThrows(PatientNotRegisteredException.class,
+                () -> noteService.getAllByPatientId("patientId not exist"));
+    }
+
+    @Test
+    void getAllByPatientId_getAllNotesOfAPatient() throws PatientNotRegisteredException {
+        //given
+        Note note1 = new Note("testId 1","testImageId","testText1");
+        Note note2 = new Note("testId 2","testImageId","testText2");
+
+        NoteRepository noteRepository = mock(NoteRepository.class);
+        when(noteRepository.findAllByImageId("testImageId")).thenReturn(List.of(note1, note2));
+
+        PatientRepository patientRepository = mock(PatientRepository.class);
+        when(patientRepository.findById("patientId"))
+                .thenReturn(Optional.of(createOnePatient()));
+
+        //when
+        NoteService noteService = new NoteService(noteRepository,patientRepository);
+        Map<String,List<Note>> actual = noteService.getAllByPatientId("patientId");
+
+        //then
+        Assertions.assertEquals(Map.of("testImageId" ,List.of(note1,note2)), actual);
+        verify(noteRepository).findAllByImageId("testImageId");
+        verify(patientRepository).findById("patientId");
+    }
+
+    @Test
+    void updateById_throwException_whenNoteDoesNotExist() {
+        //given
+        NoteRepository noteRepository = mock(NoteRepository.class);
+        when(noteRepository.existsById("testId")).thenReturn(false);
+
+        PatientRepository patientRepository = mock(PatientRepository.class);
+
+        //when
+        NoteService noteService = new NoteService(noteRepository,patientRepository);
+
+        //then
+        Assertions.assertThrows(NotFoundException.class,
+                () -> noteService.updateById("not existing id",new Note()));
+    }
+
+    @Test
+    void updateById_updateNote_whenNoteExists() throws NotFoundException {
+        //given
+        Note note = new Note("testId","testImageId","testText");
+
+        NoteRepository noteRepository = mock(NoteRepository.class);
+        when(noteRepository.existsById("testId")).thenReturn(true);
+        when(noteRepository.save(note)).thenReturn(note);
+
+        PatientRepository patientRepository = mock(PatientRepository.class);
+
+        //when
+        NoteService noteService = new NoteService(noteRepository,patientRepository);
+        Note actual = noteService.updateById("testId",note);
+
+        //then
+        Assertions.assertEquals(note, actual);
+        verify(noteRepository).existsById("testId");
+        verify(noteRepository).save(note);
+    }
+
+    @Test
+    void deleteById_throwException_whenNoteDoesNotExist() {
+        //given
+        NoteRepository noteRepository = mock(NoteRepository.class);
+        when(noteRepository.existsById("testId")).thenReturn(false);
+
+        PatientRepository patientRepository = mock(PatientRepository.class);
+
+        //when
+        NoteService noteService = new NoteService(noteRepository,patientRepository);
+
+        //then
+        Assertions.assertThrows(NotFoundException.class,
+                () -> noteService.deleteById("not existing id"));
+    }
+
+    @Test
+    void deleteById_deleteNote_whenNoteExists() throws NotFoundException {
+        //given
+        NoteRepository noteRepository = mock(NoteRepository.class);
+        when(noteRepository.existsById("testId")).thenReturn(true);
+
+        PatientRepository patientRepository = mock(PatientRepository.class);
+
+        //when
+        NoteService noteService = new NoteService(noteRepository,patientRepository);
+        noteService.deleteById("testId");
+        //then
+        verify(noteRepository).deleteById("testId");
     }
 }
